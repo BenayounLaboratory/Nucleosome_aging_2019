1742a972e4ab4a3933d66383d430cd277065b26b
Bérénice Benayoun <35042323+BenayounLaboratory@users.noreply.github.com>
2019-12-13 14:49:51 -0800

    Add files via upload

---
+++ Genome_Ontology_Analyses/CEAS_annotation/run_ceas.sh
@@ -0,0 +1,20 @@
+ceas -b ../Beds_of_changed_Nucleosomes/consensus/dposREF_OlfactoryBulb_DiNUP_DANPOS_LOST.bed    --name=OlfactoryBulb_DiNUP_DANPOS_LOST_dposRef   --sizes=5000 -g /Users/benayoun/Softwares/CEAS-Package-1.0.2/mm9.refGene
+ceas -b ../Beds_of_changed_Nucleosomes/consensus/dposREF_OlfactoryBulb_DiNUP_DANPOS_GAINED.bed  --name=OlfactoryBulb_DiNUP_DANPOS_GAINED_dposRef --sizes=5000 -g /Users/benayoun/Softwares/CEAS-Package-1.0.2/mm9.refGene
+ceas -b ../Beds_of_changed_Nucleosomes/consensus/dposREF_NPCs_DiNUP_DANPOS_LOST.bed             --name=NPCs_DiNUP_DANPOS_LOST_dposRef            --sizes=5000 -g /Users/benayoun/Softwares/CEAS-Package-1.0.2/mm9.refGene
+ceas -b ../Beds_of_changed_Nucleosomes/consensus/dposREF_NPCs_DiNUP_DANPOS_GAINED.bed           --name=NPCs_DiNUP_DANPOS_GAINED_dposRef          --sizes=5000 -g /Users/benayoun/Softwares/CEAS-Package-1.0.2/mm9.refGene
+ceas -b ../Beds_of_changed_Nucleosomes/consensus/dposREF_Liver_DiNUP_DANPOS_LOST.bed            --name=Liver_DiNUP_DANPOS_LOST_dposRef           --sizes=5000 -g /Users/benayoun/Softwares/CEAS-Package-1.0.2/mm9.refGene
+ceas -b ../Beds_of_changed_Nucleosomes/consensus/dposREF_Liver_DiNUP_DANPOS_GAINED.bed          --name=Liver_DiNUP_DANPOS_GAINED_dposRef         --sizes=5000 -g /Users/benayoun/Softwares/CEAS-Package-1.0.2/mm9.refGene
+ceas -b ../Beds_of_changed_Nucleosomes/consensus/dposREF_Heart_DiNUP_DANPOS_LOST.bed            --name=Heart_DiNUP_DANPOS_LOST_dposRef           --sizes=5000 -g /Users/benayoun/Softwares/CEAS-Package-1.0.2/mm9.refGene
+ceas -b ../Beds_of_changed_Nucleosomes/consensus/dposREF_Heart_DiNUP_DANPOS_GAINED.bed          --name=Heart_DiNUP_DANPOS_GAINED_dposRef         --sizes=5000 -g /Users/benayoun/Softwares/CEAS-Package-1.0.2/mm9.refGene
+ceas -b ../Beds_of_changed_Nucleosomes/consensus/dposREF_Cerebellum_DiNUP_DANPOS_LOST.bed       --name=Cerebellum_DiNUP_DANPOS_LOST_dposRef      --sizes=5000 -g /Users/benayoun/Softwares/CEAS-Package-1.0.2/mm9.refGene
+ceas -b ../Beds_of_changed_Nucleosomes/consensus/dposREF_Cerebellum_DiNUP_DANPOS_GAINED.bed      --name=Cerebellum_DiNUP_DANPOS_GAINED_dposRef   --sizes=5000 -g /Users/benayoun/Softwares/CEAS-Package-1.0.2/mm9.refGene
+
+
+ceas -b /Volumes/MyBook_3/BD_aging_project/ChIP-seq/All_tissues_analysis/Motif_analyses/HOMER/Consensus_Nucleosomes/Liver_H3_nuc_background_noheader.bed          --name=Liver_background_dposRef      --sizes=5000 -g /Users/benayoun/Softwares/CEAS-Package-1.0.2/mm9.refGene
+ceas -b /Volumes/MyBook_3/BD_aging_project/ChIP-seq/All_tissues_analysis/Motif_analyses/HOMER/Consensus_Nucleosomes/NPCs_H3_nuc_background_noheader.bed           --name=NPCs_background_dposRef       --sizes=5000 -g /Users/benayoun/Softwares/CEAS-Package-1.0.2/mm9.refGene
+ceas -b /Volumes/MyBook_3/BD_aging_project/ChIP-seq/All_tissues_analysis/Motif_analyses/HOMER/Consensus_Nucleosomes/OlfactoryBulb_H3_nuc_background_noheader.bed  --name=OB_background_dposRef         --sizes=5000 -g /Users/benayoun/Softwares/CEAS-Package-1.0.2/mm9.refGene
+ceas -b /Volumes/MyBook_3/BD_aging_project/ChIP-seq/All_tissues_analysis/Motif_analyses/HOMER/Consensus_Nucleosomes/Heart_H3_nuc_background_noheader.bed          --name=Heart_background_dposRef      --sizes=5000 -g /Users/benayoun/Softwares/CEAS-Package-1.0.2/mm9.refGene
+ceas -b /Volumes/MyBook_3/BD_aging_project/ChIP-seq/All_tissues_analysis/Motif_analyses/HOMER/Consensus_Nucleosomes/Cerebellum_H3_nuc_background_noheader.bed     --name=Cerebellum_background_dposRef --sizes=5000 -g /Users/benayoun/Softwares/CEAS-Package-1.0.2/mm9.refGene
+
+
+
